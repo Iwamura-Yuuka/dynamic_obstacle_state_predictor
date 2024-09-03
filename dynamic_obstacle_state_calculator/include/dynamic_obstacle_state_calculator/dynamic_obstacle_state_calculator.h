45d6405caaed70d76b6f24849cb0e638e4270294
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-09-03 17:37:00 +0900

    commit for now

--- dynamic_obstacle_state_calculator/include/dynamic_obstacle_state_calculator/dynamic_obstacle_state_calculator.h
+++ dynamic_obstacle_state_calculator/include/dynamic_obstacle_state_calculator/dynamic_obstacle_state_calculator.h
@@ -3,10 +3,14 @@
 
 #include <ros/ros.h>
 #include <queue>
+#include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Point.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/utils.h>
 
 // original_msgs
 #include <pedestrian_msgs/PersonState.h>
@@ -28,13 +32,20 @@ public:
 
 private:
     // コールバック関数
-    void obs_pose_callback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+    void obs_traj_callback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+    void obs_pose_callback(const geometry_msgs::PoseArray::ConstPtr& msg);
     
     // 引数あり関数
+    void transform_obs_pose(geometry_msgs::Point& obs_pose);  // 障害物情報をworld座標系からrobot座標系に変換
+    void calc_obs_state(const visualization_msgs::Marker& obs_trajectory);  // 1つの障害物の状態を計算
 
     // 引数なし関数
+    void update_obs_data();  // 障害物情報の更新
 
     // yamlファイルで設定可能な変数
+    int hz_;
+    std::string world_frame_;
+    std::string robot_frame_;
 
     // msgの受け取り判定用
     bool is_obs_pose_received_ = false;
@@ -44,12 +55,21 @@ private:
     ros::NodeHandle private_nh_;
 
     // Subscriber
+    ros::Subscriber sub_obs_traj_;
     ros::Subscriber sub_obs_pose_;
 
     // Publisher
+    ros::Publisher pub_obs_state_;
+    ros::Publisher pub_obs_pose_;
+
+    // tf
+    tf2_ros::TransformListener tf_listener_;
+    tf2_ros::Buffer tf_buffer_;
 
     // 各種オブジェクト
     visualization_msgs::MarkerArray obs_trajectories_;
+    geometry_msgs::PoseArray obs_poses_;
+    geometry_msgs::PoseArray tmp_obs_poses_;
 };
 
 #endif // DYNAMIC_OBSTACLE_STATE_CALCULATOR_H
--- dynamic_obstacle_state_calculator/src/dynamic_obstacle_state_calculator.cpp
+++ dynamic_obstacle_state_calculator/src/dynamic_obstacle_state_calculator.cpp
@@ -4,24 +4,58 @@ DynamicObstacleStateCalculator::DynamicObstacleStateCalculator():private_nh_("~"
 {
   // param
   private_nh_.param("hz", hz_, {10});
+  private_nh_.param("world_frame", world_frame_, {"map"});
+  private_nh_.param("robot_frame", robot_frame_, {"base_link"});
     
-  // Subscriber
-  sub_obs_pose_ = nh_.subscribe("/dynamic_obstacle/trajectories", 1, &DynamicObstacleStateCalculator::obs_pose_callback, this);
+  // subscriber
+  sub_obs_traj_ = nh_.subscribe("/dynamic_obstacle/trajectories", 1, &DynamicObstacleStateCalculator::obs_traj_callback, this, ros::TransportHints().reliable().tcpNoDelay());
+  sub_obs_pose_ = nh_.subscribe("/dynamic_obstacle/pose", 1, &DynamicObstacleStateCalculator::obs_pose_callback, this, ros::TransportHints().reliable().tcpNoDelay());
+
+  // publisher
+  pub_obs_state_ = nh_.advertise<pedestrian_msgs::PeopleStates>("/dynamic_obstacle_states/current", 1);
+
+  // debug
+  pub_obs_pose_ = nh_.advertise<geometry_msgs::PoseArray>("/dynamic_obstacle_pose/current", 1);
 }
 
 // 障害物情報のコールバック関数
-void DynamicObstacleStateCalculator::obs_pose_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
+void DynamicObstacleStateCalculator::obs_traj_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 {
   obs_trajectories_ = *msg;
   is_obs_pose_received_ = true;
 }
 
-// 1つの障害物の状態を計算
-void DynamicObstacleStateCalculator::calc_obs_state(const visualization_msgs::Marker& obs_trajectory)
+// 障害物情報をworld座標系からrobot座標系に変換
+void DynamicObstacleStateCalculator::transform_obs_pose(geometry_msgs::Point& obs_pose)
 {
+  geometry_msgs::TransformStamped tf;
+  try
+  {
+    tf = tf_buffer_.lookupTransform(robot_frame_, world_frame_, ros::Time(0));
+
+    obs_pose.x -= tf.transform.translation.x;
+    obs_pose.y -= tf.transform.translation.y;
+  }
+  catch (tf2::TransformException &ex)
+  {
+    ROS_WARN("%s", ex.what());
+    return;
+  }
+}
+
+// 1つの障害物の状態を計算
+void DynamicObstacleStateCalculator::calc_obs_state(const visualization_msgs::Marker& obs_trajectory, pedestrian_msgs::PersonState& obs)
+{
+  // 障害物の軌跡情報の後ろ2つを取得
   geometry_msgs::Point current_obs_pose = obs_trajectory.points.back();  // 障害物の現在位置を取得
   geometry_msgs::Point prev_obs_pose = obs_trajectory.points[obs_trajectory.points.size() - 2];  // 障害物の1つ前の位置を取得
-  // 障害物の軌跡情報の後ろ2つを取得
+  
+  // 障害物の位置をworld座標系からrobot座標系に変換
+  transform_obs_pose(current_obs_pose);
+  transform_obs_pose(prev_obs_pose);
+
+  // 障害物の移動速度を計算
+
 }
 
 // 障害物情報の更新
@@ -29,9 +63,13 @@ void DynamicObstacleStateCalculator::update_obs_data()
 {
   if(is_obs_pose_received_)  // 障害物情報がsubscribeできている場合，その情報をもとに処理
   {
-    for(int id = 0; id < obs_trajectories_.markers.size(); id++)
+    if(obs_trajectories_.markers.size() != 0)  // 障害物情報が1つ以上ある場合は計算
     {
-      calc_obs_state(obs_trajectories_.markers[id]);
+      for(int id = 0; id < obs_trajectories_.markers.size(); id++)
+      {
+        // 1つの障害物の状態を計算
+        calc_obs_state(obs_trajectories_.markers[id]);
+      }
     }
   }
   else
